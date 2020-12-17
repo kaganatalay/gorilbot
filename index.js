4c9a2beed25a40da424d4769a1ae48ff35713cf4
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-17 20:21:48 +0300

    added back all assets

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const client = new discord.Client();
 let { assets, description } = loader.load();
 
 client.on('ready', () => {
-    console.log('Beep boop 🤖');
+    console.log('Beep boop');
     client.user.setActivity('Yarwdım');
 });
 
 client.on('message', message => {
     let raw = message.content.toLowerCase();
-    console.log(message);
+
     if(raw == 'yarwdım') {
         const embed = new discord.MessageEmbed()
-        .setColor('#e67e22')
+        .setColor('#9b59b6')
         .setTitle('Kullanılabilir Kowmutlar')
         .setDescription(`\`\`\` ${description} \`\`\``);
 
         message.channel.send(embed);
     } else {
-        for(let image of images) {
-            if(raw == image.command) {
-                message.channel.send({files: ["./assets/" + image.url]});
+        for(let asset of assets) {
+            if(raw == asset.command) {
+                message.channel.send(asset.url);
             }
         }
     }
